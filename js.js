6fc8c3e8142815193cbc069a58c8a6a46b48afbb
Your Name <you@example.com>
2021-12-18 17:49:12 +0700

    release web band

---
+++ js.js
@@ -0,0 +1,26 @@
+
+ function open_ticketbox(){
+    document.querySelector(".modal").style.display = 'flex';
+       
+ };
+
+ 
+ function close_ticketbox(){
+  document.querySelector(".modal").style.display = 'none';
+    
+};
+
+
+
+function showsubnav(){
+  document.getElementById("subnav-mobie").style.display= "block"; 
+   }
+   
+
+function closesubnav(){
+  document.getElementById("subnav-mobie").style.display= "none"; 
+}
+
+
+
+
